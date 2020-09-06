60709608cfba8c50025921884762d01cc3e0fa6c
Aswin Kalarickal <mail@aswink.in>
2020-09-06 23:49:58 +0530

    Added custom 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Box, Image, Text, Link } from '@chakra-ui/core'
+import NextLink from 'next/link'
+import Container from '../components/Container'
+
+const Error = () => {
+  return (
+    <Container title="404 Page Not Found">
+      <Box textAlign="center">
+        <Image src={'/static/images/404.svg'} alt="404" width="100%" />
+        <Box mt={8}>
+          <Text>
+            Oops! You have reached the other end of the world... There's nothing
+            for you here!
+          </Text>
+          <Link color="pink.400">
+            <NextLink href="/">&larr; Go back home</NextLink>
+          </Link>
+        </Box>
+      </Box>
+    </Container>
+  )
+}
+
+export default Error
