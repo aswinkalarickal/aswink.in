40cec10b25e9f6bcf6a7fd00513fb0f94431b727
Aswin Kalarickal <mail@aswink.in>
2020-09-07 22:08:36 +0530

    Added links to footer nav

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import { Box, Link } from '@chakra-ui/core'
+import NextLink from 'next/link'
 
 const Footer = () => (
   <Box as="footer" textAlign="center" fontSize={14} color="gray.500" py={8}>
     <nav>
-      <Link>github</Link>
-      <Link ml={4}>linkedin</Link>
-      <Link ml={4}>instagram</Link>
+      <NextLink href="https://github.com/aswinkalarickal" passHref={true}>
+        <Link as="a">github</Link>
+      </NextLink>
+
+      <NextLink href="https://linkedin.com/in/aswinkalarickal" passHref={true}>
+        <Link as="a" ml={4}>
+          linkedin
+        </Link>
+      </NextLink>
+
+      <NextLink href="https://instagram.com/aswinkalarickal" passHref={true}>
+        <Link as="a" ml={4}>
+          instagram
+        </Link>
+      </NextLink>
     </nav>
     <div>Copyright &copy; {new Date().getFullYear()}</div>
   </Box>
